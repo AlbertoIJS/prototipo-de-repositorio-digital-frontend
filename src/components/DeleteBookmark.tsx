ab486d63dc4324cb1de048aabd53bc1175ae473f
AlbertoIJS <aijs.cdmx@gmail.com>
2025-06-10 08:26:25 -0600

    Add confirmation dialog

--- src/components/DeleteBookmark.tsx
+++ src/components/DeleteBookmark.tsx
@@ -1,17 +1,46 @@
 "use client";
 
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
 import { Trash } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { removeMaterial } from "@/lib/actions";
 
 export default function DeleteBookmark({ materialId }: { materialId: number }) {
   return (
-    <Button
-      variant="ghost"
-      size="icon"
-      onClick={() => removeMaterial(materialId)}
-    >
-      <Trash className="size-5 text-red-500" />
-    </Button>
+    <AlertDialog>
+      <AlertDialogTrigger asChild>
+        <Button variant="ghost" size="icon">
+          <Trash className="size-5 text-red-500" />
+        </Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>¿Estás seguro?</AlertDialogTitle>
+          <AlertDialogDescription>
+            Esta acción eliminará este material de forma permanente. No podrás
+            recuperarlo después de eliminarlo.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancelar</AlertDialogCancel>
+          <AlertDialogAction
+            onClick={() => removeMaterial(materialId)}
+            className="bg-red-500 hover:bg-red-600"
+          >
+            Eliminar
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
   );
 }
